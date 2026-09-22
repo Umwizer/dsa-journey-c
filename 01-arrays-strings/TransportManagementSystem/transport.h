0e47edbc866fc0bfeba051daff236a870aeccd9f
Umwizer <ruthumwizerwa@gmail.com>
2026-09-22 15:14:58 +0200

    declaring variables

---
+++ 01-arrays-strings/TransportManagementSystem/transport.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "transport.h"
+
+int vehicleID[MAX];
+char vehicleName[MAX][30];
+char route[MAX][50];
+float fare[MAX];
+int passengers[MAX];
+int count = 0;
+
+int main() {
+    // We'll call our functions here as we build them
+    return 0;
+}
